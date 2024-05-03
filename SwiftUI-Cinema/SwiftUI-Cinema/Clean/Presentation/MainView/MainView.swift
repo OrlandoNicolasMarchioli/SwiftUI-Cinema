69db7c01afbc2663d12a55f5ded8be771832a521
Nicolas <nico.marchioli@gmail.com>
2024-05-03 10:32:10 -0300

    Adding clean structure

---
+++ SwiftUI-Cinema/SwiftUI-Cinema/Clean/Presentation/MainView/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  SwiftUI-Cinema
+//
+//  Created by Orlando Nicolas Marchioli on 03/05/2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ SwiftUI-Cinema/SwiftUI-Cinema/SwiftUI_CinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CinemaApp.swift
+//  SwiftUI-Cinema
+//
+//  Created by Orlando Nicolas Marchioli on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
